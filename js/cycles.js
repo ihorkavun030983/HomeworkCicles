60bec4e57a31fc8b4d75d2d7afabec8023cb134e
ihorkavun030983 <ihorkavun1234@gmail.com>
2023-10-08 16:39:46 +0300

    Cicles

---
+++ js/cycles.js
@@ -0,0 +1,90 @@
+//1  в один рядок через кому числа від 10 до 20.
+
+// let cycleNum = '';
+// for (let i = 10; i <= 20; i++) {
+// 	cycleNum += i + ', ';
+// }
+// console.log(cycleNum);
+
+// 2 квадрати чисел від 10 до 20
+
+// let firstNum = 10;
+// let degreeNum = 2;
+// for(firstNum = 10; firstNum <= 20; firstNum++){
+//   console.log(firstNum ** degreeNum)
+// }
+
+// 3 множення на 7
+
+// let multipliedNumSeven
+// let multiplierNum
+// for (multipliedNumSeven = 1; multipliedNumSeven <= 100; multipliedNumSeven++)
+// for (multiplierNum = 7; multiplierNum <= 7; multiplierNum++)
+// console.log(+multipliedNumSeven+" * "+multiplierNum+" = "+(multipliedNumSeven*multiplierNum));
+
+
+//4 сума чисел від 1 до 15
+
+// let sumNum = 0;
+// for(let i=0; i<=15; i++){
+//     sumNum = sumNum + i
+// }
+//  console.log(sumNum)
+
+//5 добуток чисел від 15 до 35
+//  let multipNum = 15; 
+//  for(let i=15; i<=35; i++){ 
+//      
+//  }
+// console.log(multipNum * i )
+
+//6 середнє арифметичне чисел від 1 до 500
+
+// let divNum = 1;
+// for(let i=1; i<=500; i++){
+//     divNum = divNum + i
+// }
+// console.log(divNum/500)
+
+//7 сума парних чисел від 30 до 80
+// summNum = 0
+//  for(let sumNumber = 30; sumNumber <= 80; sumNumber++){
+//   if(sumNumber % 2 == 0)
+//    summNum += sumNumber;
+//  }
+//  console.log(summNum);
+ 
+
+//8 числа в діапазоні від 100 до 200 кратні 3.
+
+// for (let multiThree = 100; multiThree < 200; multiThree++) {
+//   if (multiThree % 3 == 0) 
+//   console.log(multiThree);
+// }
+
+// 9 всі дільники числа 150/ 10 кількість дільників/ 11 сума парних дільників
+
+// let naturalNum = 150;
+// let evenDiv = 0,
+//   summNum = 0;
+// for (firstNum = 0; firstNum <= naturalNum; firstNum++) {
+//   if (naturalNum % firstNum == 0) {
+//     document.write(`Дільники числа: ${firstNum} <br /><br />`);
+
+//     if (firstNum % 2 === 0) {
+//       evenDiv++;
+//       summNum += firstNum;
+//     }
+//   }
+// }
+
+// document.write(`Кількість парних дільників: ${evenDiv} <br /><br />`);
+// document.write(`Сума парних дільників: ${summNum} <br /><br />`);
+
+//12 таблиця множення від 1 до 10
+// let firstNum = 1; 
+// let secondNum = 10;
+// for (let firstNum=1; firstNum<=10; firstNum++){
+// for (let secondNum=1; secondNum<=10; secondNum++)
+// document.write("<p>"+firstNum+"*"+secondNum+" = "+(firstNum*secondNum)+"</p>");
+// }
